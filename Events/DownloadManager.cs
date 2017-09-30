c7d30890a928ca9a42267da41b3b653d18860c71
Cristian Merli <stesvis@gmail.com>
2017-09-30 13:10:59 -0600

    first commit

---
+++ Events/DownloadManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Events
+{
+    internal class DownloadManager
+    {
+        public event EventHandler DownloadCompleted;
+
+        public DownloadManager()
+        {
+        }
+
+        public void DownloadFile(string filename)
+        {
+            Console.WriteLine($"Downloading file {filename}");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+
+            OnDownloadCompleted(filename);
+        }
+
+        protected virtual void OnDownloadCompleted(string filename)
+        {
+            //Console.WriteLine("Handling event");
+            DownloadCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var manager = new DownloadManager();
+            manager.DownloadCompleted += Manager_DownloadCompleted1;
+
+            manager.DownloadCompleted += Manager_DownloadCompleted2;
+
+            manager.DownloadFile("test.png");
+
+            //System.Console.ReadKey();
+        }
+
+        private static void Manager_DownloadCompleted1(object sender, System.EventArgs e)
+        {
+            System.Console.WriteLine("The download is complete (1)");
+        }
+
+        private static void Manager_DownloadCompleted2(object sender, System.EventArgs e)
+        {
+            System.Console.WriteLine("The download is complete (2)");
+        }
+    }
+}
